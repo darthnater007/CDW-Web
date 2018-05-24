bf3a31cd9a28101ac8e47cb619b0f586d2c6bf85
darthnater007 <nsperk@gmail.com>
2018-05-24 14:50:50 -0400

    Added piece entity class-- nothing tested yet, still need repository and controller before postman tests

---
+++ src/main/java/com/cdw/business/piece/Piece.java
@@ -0,0 +1,123 @@
+package com.cdw.business.piece;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.cdw.business.user.User;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+public class Piece {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@ManyToOne //is this a many to one or one to many instance
+	@JoinColumn(name="UserId")
+	private User user;
+	private String title;
+	private String genre;
+	private String fileName;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm")
+	private Timestamp submitted;
+	@Column(name = "IsPublication")
+	private boolean publication;
+	
+	public Piece() {
+		
+	}
+
+	public Piece(User user, String title, String genre, String fileName, Timestamp submitted, boolean publication) {
+		super();
+		this.user = user;
+		this.title = title;
+		this.genre = genre;
+		this.fileName = fileName;
+		this.submitted = submitted;
+		this.publication = publication;
+	}
+
+	public Piece(int id, User user, String title, String genre, String fileName, Timestamp submitted,
+			boolean publication) {
+		super();
+		this.id = id;
+		this.user = user;
+		this.title = title;
+		this.genre = genre;
+		this.fileName = fileName;
+		this.submitted = submitted;
+		this.publication = publication;
+	}
+
+	@Override
+	public String toString() {
+		return "Piece [id=" + id + ", user=" + user + ", title=" + title + ", genre=" + genre + ", fileName=" + fileName
+				+ ", submitted=" + submitted + ", publication=" + publication + "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public Timestamp getSubmitted() {
+		return submitted;
+	}
+
+	public void setSubmitted(Timestamp submitted) {
+		this.submitted = submitted;
+	}
+
+	public boolean isPublication() {
+		return publication;
+	}
+
+	public void setPublication(boolean publication) {
+		this.publication = publication;
+	}
+	
+	
+
+}
