6694921cb011c2ba9bcfc92f31de010d3b9d68f8
darthnater007 <nsperk@gmail.com>
2018-05-29 20:11:37 -0400

    Forgot to add annotations in the Event.java class

--- src/main/java/com/cdw/business/event/Event.java
+++ src/main/java/com/cdw/business/event/Event.java
@@ -3,8 +3,15 @@ package com.cdw.business.event;
 import java.sql.Date;
 import java.sql.Time;
 
-public class Event {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Event {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String hostedBy;
 	private String description;
