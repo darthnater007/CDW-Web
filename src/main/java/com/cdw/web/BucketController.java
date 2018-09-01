c294f0db832cdad81972e52cf9dba7247cef0286
darthnater007 <nsperk@gmail.com>
2018-09-01 16:56:53 -0400

    changed everything back, as the @controller annotation appeared to be the problem

--- src/main/java/com/cdw/web/BucketController.java
+++ src/main/java/com/cdw/web/BucketController.java
@@ -1,18 +1,18 @@
 package com.cdw.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.cdw.business.piece.AmazonClient;
 
 @CrossOrigin
-@Controller
+@RestController
 	@RequestMapping("/Pieces")
 	public class BucketController {
 
