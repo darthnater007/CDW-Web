c1377bdefefa3c7957c8ef037787e5e3d7bf6984
darthnater007 <nsperk@gmail.com>
2018-09-01 18:01:30 -0400

    still fixing remove file method--tried @requerstParam to avoid sending anything in the body of the http request

--- src/main/java/com/cdw/web/BucketController.java
+++ src/main/java/com/cdw/web/BucketController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -31,7 +33,7 @@ import com.cdw.business.piece.AmazonClient;
 	    }
 
 	    @DeleteMapping("/RemoveFile")
-	    public String[] deleteFile(@RequestPart(value = "fileName") String fileName) {
+	    public String[] deleteFile(@RequestParam String fileName) {
 	    	String[] rtrnArr = new String[1];
 	        rtrnArr[0] = this.amazonClient.deleteFileFromS3Bucket(fileName);
 	        return rtrnArr;
